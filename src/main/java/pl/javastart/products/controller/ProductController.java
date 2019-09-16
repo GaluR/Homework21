2417d7a96452dd32c9839a8bf584c1e4672517be
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-09-16 14:59:30 +0200

    Change methods, add html files,

--- src/main/java/pl/javastart/products/controller/ProductController.java
+++ src/main/java/pl/javastart/products/controller/ProductController.java
@@ -1,6 +1,7 @@
 package pl.javastart.products.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +14,6 @@ import java.util.List;
 @Controller
 public class ProductController {
 
-
     private ProductList productList;
 
     public ProductController(ProductList productList) {
@@ -25,50 +25,62 @@ public class ProductController {
     public String showAllProducts(@RequestParam Category category) {
         List<Product> all = ProductList.getAll();
         String result = "";
-        if (!category.getName().equals(null) && !"".equals(category)) {
-            switch (category) {
-                case FOOD:
-                    for (Product product : all) {
-                        if (product.getCategory().equals(Category.FOOD)) {
-                            result += product.toString() + "<br/>";
-                        }
-                    }
-                    break;
-                case OTHERS:
-                    for (Product product : all) {
-                        if (product.getCategory().equals(Category.OTHERS)) {
-                            result += product.toString() + "<br/>";
-                        }
-                    }
-                    break;
-                case ITEMS:
-                    for (Product product : all) {
-                        if (product.getCategory().equals(Category.ITEMS)) {
-                            result += product.toString() + "<br/>";
-                        }
-                    }
-                    break;
-                default:
-                    for (Product product : all) {
-                        result += product.toString() + "<br/>";
-                    }
-                    break;
-            }
-            return result;
-        } else {
-            for (Product product : all) {
-                result += product.toString() + "<br/>";
-            }
-            return result;
+        double sum = 0;
+        switch (category) {
+            case FOOD:
+                result = getProducts(all, result, Category.FOOD);
+                sum = getSum(all, sum, Category.FOOD);
+                break;
+            case OTHERS:
+                result = getProducts(all, result, Category.OTHERS);
+                sum = getSum(all, sum, Category.OTHERS);
+                break;
+            case ITEMS:
+                result = getProducts(all, result, Category.ITEMS);
+                sum = getSum(all, sum, Category.ITEMS);
+                break;
+            default:
+                for (Product product : all) {
+                    result += product.toString() + "<br/>";
+                }
+                break;
         }
+        return result + "<br/> Sumaryczna cena produktów z listy to: " + sum;
     }
 
 
-    @RequestMapping("/products1")
-    public double finallyPrice() {
-        double price = productList.getProduct().getPrice();
-        return price;
+    private String getProducts(List<Product> all, String result, Category items) {
+        for (Product product : all) {
+            if (product.getCategory().equals(items)) {
+                result += product.toString() + "<br/>";
+            }
+        }
+        return result;
+    }
 
+    private double getSum(List<Product> all, double sum, Category food) {
+        for (int i = 0; i < all.size(); i++) {
+            if (all.get(i).getCategory().equals(food)) {
+                sum += all.get(i).getPrice();
+            }
+        }
+        sum *= 100;
+        Math.round(sum);
+        sum /= 100;
+
+        return sum;
+    }
+
+
+    @RequestMapping("/add")
+    public String add(@RequestParam(defaultValue = "Anonim", required = false) String nazwa, @RequestParam double cena, @RequestParam Category kategoria) {
+        if (StringUtils.isEmpty(kategoria)) {
+            return "redirect:err.html";
+        } else {
+            Product product = new Product(nazwa, cena, kategoria);
+            ProductList.getAll().add(product);
+            return "redirect:success.html";
+        }
     }
 }
 
